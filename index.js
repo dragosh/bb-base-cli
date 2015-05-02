44174fb1764fb6dd8293cf12dc8ac24a9f2d10b9
dragosh <oancea.dragosh@gmail.com>
2015-05-02 12:50:06 +0200

    add event emitter on cli and cmd

--- index.js
+++ index.js
@@ -4,22 +4,23 @@
 /*----------------------------------------------------------------*/
 
 /**
- * [createCli description]
- * @param  {[type]}   name   [description]
- * @param  {[type]}   params [description]
- * @param  {Function} cb     [description]
- * @return {[type]}          [description]
+ * Create CLI object instance
+ * @param  {string}   name   CLI name
+ * @param  {object}   params  options/arguments/description
+ * @param  {Function} cb     Callback
+ * @return {object}          cliparser object
  */
 exports.createCli = function(name, params, cb) {
     var CLI = require('./lib/cli');
     return new CLI(name, params, cb);
 };
+
 /**
- * [createCmd description]
- * @param  {[type]}   name   [description]
- * @param  {[type]}   params [description]
- * @param  {Function} cb     [description]
- * @return {[type]}          [description]
+ * Create Commands/subcommands
+ * @param  {string}   name   CMD name
+ * @param  {obect}   params options/arguments/description
+ * @param  {Function} cb     Callback
+ * @return {object}          cliparser object
  */
 exports.createCmd = function(name, params, cb) {
     var CMD = require('./lib/cmd');
@@ -27,8 +28,7 @@ exports.createCmd = function(name, params, cb) {
 };
 
 /**
- * [readDir description]
- * @param  {[type]} currDir [description]
- * @return {[type]}         [description]
+ * Lodash + base utils
+ * @type {function}
  */
 exports.utils = require('./lib/utils');
